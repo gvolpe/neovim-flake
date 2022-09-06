e8d12f3292960d8990cf18a03ab4d82bf24cc6e3
Gabriel Volpe <volpegabriel@gmail.com>
2022-09-06 09:39:15 +0200

    migrate glow.nvim config to lua

--- modules/markdown/glow.nix
+++ modules/markdown/glow.nix
@@ -21,10 +21,18 @@ in
     vim.startPlugins =
       withPlugins cfg.glow.enable [ pkgs.neovimPlugins.glow-nvim ];
 
+    vim.luaConfigRC = writeIf cfg.glow.enable ''
+      require('glow').setup({
+        glow_path = "${pkgs.glow}/bin/glow",
+        border = "shadow", 
+        pager = true,
+        width = 120,
+      })
+    '';
+
     vim.configRC =
       writeIf cfg.glow.enable ''
         autocmd FileType markdown noremap <leader>p :Glow<CR>
-        let g:glow_binary_path = "${pkgs.glow}/bin/glow"
       '';
   };
 }
