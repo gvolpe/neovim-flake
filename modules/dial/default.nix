539db764c9abe92a588c48ff75281ab205c8d954
Gabriel Volpe <volpegabriel@gmail.com>
2023-03-30 11:30:44 +0200

    dial.nvim: add scala specific config

--- modules/dial/default.nix
+++ modules/dial/default.nix
@@ -24,6 +24,13 @@ in
           augend.integer.alias.hex,
           augend.constant.alias.bool,
         },
+        scala = {
+          augend.integer.alias.decimal,
+          augend.integer.alias.hex,
+          augend.constant.alias.bool,
+          augend.constant.new{ elements = {"val", "def"} },
+          augend.constant.new{ elements = {"object", "class", "trait", "enum"} },
+        },
       }
 
       vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal(), {noremap = true})
@@ -33,5 +40,11 @@ in
       vim.keymap.set("v", "g<C-a>",require("dial.map").inc_gvisual(), {noremap = true})
       vim.keymap.set("v", "g<C-x>",require("dial.map").dec_gvisual(), {noremap = true})
     '';
+
+    vim.configRC = ''
+      autocmd FileType scala lua vim.api.nvim_buf_set_keymap(0, "n", "<C-a>", require("dial.map").inc_normal("scala"), {noremap = true})
+      autocmd FileType scala lua vim.api.nvim_buf_set_keymap(0, "n", "<C-x>", require("dial.map").dec_normal("scala"), {noremap = true})
+    '';
+
   };
 }
