0268c6a3460efd13cf91c283b48416d2c5805b99
Gabriel Volpe <volpegabriel@gmail.com>
2024-04-05 14:22:47 +0200

    harpoon: override mappings to delete without prompt

--- modules/harpoon/default.nix
+++ modules/harpoon/default.nix
@@ -22,14 +22,33 @@ in
       require('harpoon'):setup()
 
       ${writeIf tele.enable ''
-        vim.keymap.set("n", "<leader>hl", function() require("telescope").extensions.harpoon.marks() end, { desc = "List" })
+        local themes = require("telescope.themes")
+        local hm_actions = require("telescope._extensions.harpoon_marks.actions")
+
+        vim.keymap.set(
+          "n",
+          "<leader>hl",
+          function()
+            require("telescope").extensions.harpoon.marks(themes.get_dropdown({
+                previewer = false,
+                layout_config = { width = 0.6 },
+                path_display = { truncate = 10 },
+                attach_mappings = function(_, map)
+                    map("i", "<c-d>", hm_actions.delete_mark_selections)
+                    map("n", "<c-d>", hm_actions.delete_mark_selections)
+                    return true
+                end,
+            }))
+          end,
+          { desc = "List" }
+        )
       ''}
 
       ${writeIf keys.enable ''
         wk.register({
           ["<leader>h"] = {
             name = "Harpoon",
-            a = { "<cmd>lua require('harpoon'):list():append()<CR>", "Add" },
+            a = { "<cmd>lua require('harpoon'):list():add()<CR>", "Add" },
             d = { "<cmd>lua require('harpoon'):list():remove()<CR>", "Del" },
           },
         })
