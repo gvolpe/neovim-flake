f2f1f84cf7dfea0ef469fff1a604f51d2defdf42
Gabriel Volpe <volpegabriel@gmail.com>
2025-01-06 19:50:50 +0100

    do not notify about warning messages

--- modules/visuals/visuals.nix
+++ modules/visuals/visuals.nix
@@ -184,7 +184,7 @@ in
                 enabled = false, -- enables the Noice messages UI
                 view = "notify", -- default view for messages
                 view_error = "notify", -- view for errors
-                view_warn = "notify", -- view for warnings
+                view_warn = false, -- view for warnings
                 view_history = "messages", -- view for :messages
                 view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
               },
