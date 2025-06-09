af8a0413024afd976b58ccf4f027f3ba383eda93
Gabriel Volpe <volpegabriel@gmail.com>
2025-06-09 17:02:25 +0200

    harpoon: remove marks extension

--- modules/harpoon/default.nix
+++ modules/harpoon/default.nix
@@ -21,29 +21,6 @@ in
     vim.luaConfigRC = ''
       require('harpoon'):setup()
 
-      ${writeIf tele.enable ''
-        local themes = require("telescope.themes")
-        local hm_actions = require("telescope._extensions.harpoon_marks.actions")
-
-        vim.keymap.set(
-          "n",
-          "<leader>hl",
-          function()
-            require("telescope").extensions.harpoon.marks(themes.get_dropdown({
-                previewer = false,
-                layout_config = { width = 0.6 },
-                path_display = { truncate = 10 },
-                attach_mappings = function(_, map)
-                    map("i", "<c-d>", hm_actions.delete_mark_selections)
-                    map("n", "<c-d>", hm_actions.delete_mark_selections)
-                    return true
-                end,
-            }))
-          end,
-          { desc = "List" }
-        )
-      ''}
-
       ${writeIf keys.enable ''
         wk.register({
           ["<leader>h"] = {
